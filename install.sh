a45684d3ffed6022e260116e8506cb15889c6962
Claudio Wunder <cwunder@gnome.org>
2020-08-14 13:10:56 +0000

    fix: forgot to remove old os detection script

--- install.sh
+++ install.sh
@@ -47,40 +47,6 @@ case "$(uname -s)" in
     ;;
 esac
 
-# check if environment is macOS
-if [ "$(uname)" == "Darwin" ]; then
-
-# check if environment is debian/ubuntu
-elif [ "$(grep -Ei 'debian|buntu|mint' /etc/*release)" ]; then
-    echo "\e[32m[DOT]\e[34m debian based environment detected ... \n"
-    # install required dependencies 
-    echo "\e[32m[DOT]\e[34m installing packages ... \n"
-    sudo apt -y install build-essential git debconf locales fonts-hack-ttf > /dev/null 2>&1
-    # generate utf-8 environment
-    echo "\e[32m[DOT]\e[34m generating locales ... \n"
-    sudo locale-gen --purge en_US.UTF-8 > /dev/null 2>&1
-    # reloads font cache
-    echo "\e[32m[DOT]\e[34m rebuilding fonts ... \n"
-    fc-cache -f -v > /dev/null 2>&1
-# check if environment is fedora/redhat
-elif [ "$(grep -Ei 'fedora|redhat|centos' /etc/*release)" ]; then
-    echo "\e[32m[DOT]\e[34m redhat based environment detected ... \n"
-    # install required dependencies
-    echo "\e[32m[DOT]\e[34m installing packages ... \n"
-    sudo dnf copr enable zawertun/hack-fonts -y > /dev/null 2>&1
-    # install font packages
-    sudo dnf install hack-fonts @development-tools git -y > /dev/null 2>&1
-    # generate utf-8 environment
-    echo "\e[32m[DOT]\e[34m generating locales ... \n"
-    localedef -v -c -i en_US -f UTF-8 en_US.UTF-8 > /dev/null 2>&1
-    # reloads font cache
-    echo "\e[32m[DOT]\e[34m rebuilding fonts ... \n"
-    fc-cache -f -v > /dev/null 2>&1
-else
-    echo "\e[32m[DOT]\e[31m your running OS was not recognised. Exiting. \e[39m\n"
-    exit 1
-fi
-
 # exports the language definitions
 echo "\e[32m[DOT]\e[34m exporting locales ... \n"
 export LANG="en_US.UTF-8"
